686dbb0d98deda3674b4ca9d7f7dc87244c14278
icanpewpew <mypeda@gmail.com>
2015-05-19 20:59:00 +0200

    form1 -> main

--- main.cs
+++ main.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace Adventure_Capitalist_Calculator
 {
-    public partial class Form1 : Form
+    public partial class main : Form
     {
-        public Form1()
+        public main()
         {
             InitializeComponent();
         }
